8f95b6882cea035c7d5e430272ebc576fdaa6e95
Gonzalo <gon_e13@hotmail.com>
2018-11-09 17:38:35 -0300

    grafiquitos

--- Gonzalo/Match.m
+++ Gonzalo/Match.m
@@ -12,37 +12,37 @@ b  = -1.6326413*10^-6;
 T0 = 0;
 R0 = 100.07271;
 k  = 1;
+M=[];
 f=@(x) T0 + (-R0*a+((R0*a)^2-4*b*R0*(R0-x))^(1/2))/(2*R0*b);
 if LT <= LR
-%    M = zeros(LT,3);
     for i = 1:LT
-        for j=1:LR-1;
-            if Temp(i,1) > Res(j,1) && Temp(i,1) < Res(j+1,1)
-                if (Temp(i,1)-Res(j,1)) <= (Res(j+1,1)-Temp(i,1))
-                    M(k,:) = [Temp(i,1),f(Temp(i,2)),Res(j,q)*Patron];
-                end
-                if (Temp(i,1)-Res(j,1)) > (Res(j+1,1)-Temp(i,1))
-                    M(k,:) = [Temp(i,1),f(Temp(i,2)),Res(j+1,q)*Patron];
-                end
-                k=k+1;
-            end
-        end
-    end
-else
-%    M = zeros(LR,3);
+      for j=1:LR-1;
+        if Temp(i,1) > Res(j,1) && Temp(i,1) < Res(j+1,1)
+          if (Temp(i,1)-Res(j,1)) <= (Res(j+1,1)-Temp(i,1))
+            M(k,:) = [Temp(i,1),f(Temp(i,2)),Res(j,q)*Patron];
+            endif
+          if (Temp(i,1)-Res(j,1)) > (Res(j+1,1)-Temp(i,1))
+            M(k,:) = [Temp(i,1),f(Temp(i,2)),Res(j+1,q)*Patron];
+            endif
+          k=k+1;
+          endif
+        endfor
+      endfor
+    else
     for i = 1:LR
-        for j=1:LT-1;
-            if Res(i,1) > Temp(j,1) && Res(i,1) < Temp(j+1,1)
-                if (Res(i,1)-Temp(j,1)) <= (Temp(j+1,1)-Res(i,1))
-                    M(k,:) = [Res(i,1),f(Temp(j,2)),Res(i,q)*Patron];
-                end
-                if (Res(i,1)-Temp(j,1)) > (Temp(j+1,1)-Res(i,1))
-                    M(k,:) = [Res(i,1),f(Temp(j+1,2)),Res(i,q)*Patron];
-                end
-                k=k+1;
-            end
-        end        
-    end
-end
+      for j=1:LT-1;
+        if Res(i,1) > Temp(j,1) && Res(i,1) < Temp(j+1,1)
+          if (Res(i,1)-Temp(j,1)) <= (Temp(j+1,1)-Res(i,1))
+            M(k,:) = [Res(i,1),f(Temp(j,2)),Res(i,q)*Patron];
+            endif
+          if (Res(i,1)-Temp(j,1)) > (Temp(j+1,1)-Res(i,1))
+            M(k,:) = [Res(i,1),f(Temp(j+1,2)),Res(i,q)*Patron];
+            endif
+          k=k+1;
+          endif
+        endfor        
+      endfor
+    endif
 
-end
+endfunction
+
